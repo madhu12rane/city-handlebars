d6bf88bb47dd1be20a04f4ffe48ad5d513704e9d
Manasi Rane <madhu12rane@gmail.com>
2018-12-22 13:19:07 +0530

    new

---
+++ index.js
@@ -0,0 +1,10 @@
+var express=require('express');
+var exphbs = require('express-handlebars');
+var app=express();
+var routes=require('./routes/route.js');
+app.use(express.static('public',{root:'.'}));
+app.set('view engine','handlebars');
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
+app.get('/',routes.home);
+  app.get('/:city',routes.city);
+app.listen(8080);
